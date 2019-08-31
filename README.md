0fc31b27637f56a679b5408c5d5d01ccd5cb83fb
Andre H. Beckedorf <evilJazz@katastrophos.net>
2019-08-31 13:03:12 +0200

    Initial version of the script along with documentation.

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # classicmac-pcsetup-kit
 
+This script creates image files suitable for the [DOS/Windows/PC compatibility cards](http://www.oliver-schubert.com/DOScard/DOScard.html) in classic Macs. It creates image files that are readable and bootable by [PC Setup](http://www.oliver-schubert.com/DOScard/DOScard.html#pcsetup2x) (in case your floppy drive is dead like mine was).
+
+Developed and tested on Ubuntu 18.04 and PowerMac 6100, 264 MB RAM, OS 9.1, NuBus PC compatibility card, Cyrix 486 DX2 66, 32 MB RAM, PC Setup 2.1.7f only
+
+![alt text](assets/PCSetup2.png "PC Setup 2")
+
+
+## Image format
+
+The image files produced by the PC Setup Mac app are in raw format, however there is a catch: they have to be properly addressed with the correct disk geometry parameters to get working and bootable images files.
+
+The [CHS](https://en.wikipedia.org/wiki/Cylinder-head-sector) disk geometry parameters of the emulated SCSI drive are:
+
+    Cylinders = size of image in MB
+    Heads = 64
+    Sectors = 32
+
+The first partition starts at sector 32 instead of sector 63 or 2048 of more modern systems.
+
+This is the main culprit when trying to install DOS or Windows in a virtual machine: The VM also has to emulate the same disk geometry that the emulated SCSI drive of the PC Setup software is using. Failing to do so will result in the disk simply not booting due to the wrong disk geometry parameters saved in the MBR and boot sector by the DOS/Windows setup.
+
+## VirtualBox
+
+Most modern software have already removed support for the ancient CHS disk geometry. For instance I was not able to get bootable images with qemu. However, trusty VirtualBox to the rescue.
+
+The pcsetup-kit script will automatically create a .vmdk adapter file that can be used as drive image in VirtualBox. Simply create a new virtual machine in VirtualBox and make sure to select `DOS` or `Windows 95/98` as machine type and configure it similar to your PC compatibility card.
+
+Once the image is finished, copy it over to your Mac or mount the image via AFP. Make sure to change the filetype to Creator: "D:>" and Type: "APPL", so the image file will be selectable in the PC Setup app. For details see [Using images on the Mac](#using-images-on-the-mac) below.
+
+## Usage
+
+    $ ./pcsetup-kit.sh
+    Usage: ./pcsetup-kit.sh ACTION ...
+
+    ACTIONs:
+         create  -  Create a new image file
+        adapter  -  Create a VMDK adapter for an existing image file
+
+### Creating an image
+
+    $ ./pcsetup-kit.sh create
+    Usage: ./pcsetup-kit.sh create (PCSetup image filename) (Size In MB) [options]
+
+    Options:
+        --no-partiioning   Only create image file. Do not create partitions in image.
+        --no-format        Do not format image file.
+        --preseed          Copy files in files sub-directory to image.
+        --no-vmdk-file     Do not create an VMDK adapter file for Virtual Box or VMware.
+
+If you wish to create an image with VMDK-adapter file that is not initialized:
+
+    $ ./pcsetup-kit.sh create DOSStartImage.img 500 --no-partitioning
+
+This command produces a 500 MB raw image file `DOSStartImage.img` that can be used with PC Setup on the Mac. It also creates a VMDK adapter file `DOSStartImage.img.vmdk` which can be used as disk image in VirtualBox and likely VMware (untested).
+
+`--no-partitioning` makes sure to not create a partition table and force the OS setup to deal with partitioning. When the partition table is prepared beforehand, MS-DOS will most likely not boot, since the MS-DOS installer skips the steps to install the MBR and boot sector.
+
+### Creating an VMDK adapter file from an existing image
+
+    $ ./pcsetup-kit.sh adapter
+    Usage: ./pcsetup-kit.sh adapter [PCSetup image filename]
+
+If you wish to boot your already existing image file create by PC Setup on the Mac, you need an VMDK adapter file in order to use the image as disk image in VirutalBox.
+
+    $ ./pcsetup-kit.sh adapter "My Windows 95 setup.img"
+
+This will create the adapter file `My Windows 95 setup.img.vmdk`.
+
+### Using images on the Mac
+
+In order to make our created image files selectable in the PC Setup application, we need to change the resource fork information of the image to the proper Mac file type.
+This can be achieved using the app [FileType](http://www.frederikseiffert.de/filetype/). Creator: "D:>" and Type: "APPL"
+
+![alt text](assets/FileType.png "FileType.app")
---
+++ pcsetup-kit.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+#
+# PCSetup-Kit
+#
+# Copyright (C) 2019 Andre Beckedorf
+#       <evilJazz _AT_ katastrophos _DOT_ net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+SCRIPT_FILENAME=$(readlink -f "`cd \`dirname \"$0\"\`; pwd`/`basename \"$0\"`")
+SCRIPT_ROOT=$(dirname "$SCRIPT_FILENAME")
+
+set -e -o pipefail
+
+usage()
+{
+   echo "Usage: $0 ACTION ..."
+   echo
+   echo "ACTIONs:"
+   echo "         create  -  Create a new image file"
+   echo "        adapter  -  Create a VMDK adapter for an existing image file"
+   echo
+}
+
+[ $# == 0 ] && usage && exit 1
+
+getUUID()
+{
+   if [ $(which uuidgen) != "" ]; then
+      uuidgen
+   elif [ -f "/proc/sys/kernel/random/uuid" ]; then
+      cat cat /proc/sys/kernel/random/uuid
+   else
+      echo "NONE"
+   fi
+}
+
+createImageAction()
+{
+   usage()
+   {
+      echo "Usage: $0 create (PCSetup image filename) (Size In MB) [options]"
+      echo
+      echo "Options:"
+      echo "     --no-partiioning   Only create image file. Do not create partitions in image."
+      echo "     --no-format        Do not format image file."
+      echo "     --preseed          Copy files in "files" sub-directory to image."
+      echo "     --no-vmdk-file     Do not create an VMDK adapter file for Virtual Box or VMware."
+   }
+
+   [ $# -lt 2 ] && usage && exit 1
+   
+     
+   OUTFILE=$1
+   OUTFILE_MBSIZE=$2
+
+   IMAGE_INITIALIZATION=1
+   IMAGE_FORMAT=1
+   IMAGE_PRESEED=0
+   IMAGE_VMDK_ADAPTER=1
+
+   for i in "$@"; do
+      case $i in
+         --no-partitioning)
+            IMAGE_INITIALIZATION=0
+            IMAGE_FORMAT=0
+            ;;
+         --no-format)
+            IMAGE_FORMAT=0
+            ;;
+         --preseed)
+            IMAGE_PRESEED=1
+            ;;
+         --no-vmdk-file)
+            IMAGE_VMDK_ADAPTER=0
+            ;;
+         *)
+            ;;
+      esac
+   done
+
+   dd if=/dev/zero of="$OUTFILE" bs=1M count=$OUTFILE_MBSIZE
+
+   if [ $IMAGE_INITIALIZATION -eq 1 ]; then
+      sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk -c=dos -H 64 -S 32 "$OUTFILE"
+o # clear the in memory partition table
+n # new partition
+p # primary partition
+1 # partition number 1
+  # default - start at beginning of disk 
+  # default - use all remaining space
+t # Change partition type
+6 # FAT16
+a # make a partition bootable
+p # print the in-memory partition table
+w # write the partition table
+q # and we're done
+EOF
+
+      LOOPDEV=$(sudo losetup -f -P --show "$OUTFILE")
+      LOOPPART="${LOOPDEV}p1"
+
+      set -x
+
+      if [ $IMAGE_FORMAT -eq 1 ]; then
+         sudo mkdosfs -F16 -v "$LOOPPART"
+
+         if [ $IMAGE_PRESEED -eq 1 ]; then
+            sudo mount "$LOOPPART" /mnt/temp
+            sudo cp -r files/* /mnt/temp/
+            echo Press enter to unmount /mnt/temp...
+            read
+            sudo umount /mnt/temp
+         fi
+      fi
+
+      sudo losetup -d "$LOOPDEV"
+   fi
+
+   set +x
+
+   if [ $IMAGE_VMDK_ADAPTER == 1 ]; then
+      createVMDKAdapterAction "$OUTFILE"
+   fi
+}
+
+createVMDKAdapterAction()
+{
+   usage()
+   {
+      echo "Usage: $0 adapter [PCSetup image filename]"
+      echo
+   }
+
+   [ $# != 1 ] && usage && exit 1
+
+   IMAGEFILE=$1
+   IMAGEFILE_BASE=$(basename "$IMAGEFILE")
+   IMAGEFILE_SIZE=$(stat -c%s "$IMAGEFILE")
+   IMAGEFILE_SECTORS=$(($IMAGEFILE_SIZE / 512))
+   IMAGEFILE_MBSIZE=$(($IMAGEFILE_SIZE / 1024 / 1024))
+
+   OUTFILE="$IMAGEFILE.vmdk"
+
+   cat > "$IMAGEFILE.vmdk" << VMDK
+# Disk DescriptorFile
+version=1
+CID=$(getUUID | cut -d"-" -f1)
+parentCID=ffffffff
+createType="fullDevice"
+
+# Extent description
+RW $IMAGEFILE_SECTORS FLAT "$IMAGEFILE_BASE" 0
+
+# The disk Data Base 
+#DDB
+
+ddb.virtualHWVersion = "4"
+ddb.adapterType="ide"
+ddb.geometry.cylinders="$IMAGEFILE_MBSIZE"
+ddb.geometry.heads="64"
+ddb.geometry.sectors="32"
+ddb.uuid.image="$(getUUID)"
+ddb.uuid.parent="00000000-0000-0000-0000-000000000000"
+ddb.uuid.modification="$(getUUID)"
+ddb.uuid.parentmodification="00000000-0000-0000-0000-000000000000"
+ddb.geometry.biosCylinders="$IMAGEFILE_MBSIZE"
+ddb.geometry.biosHeads="64"
+ddb.geometry.biosSectors="32"
+VMDK
+
+   echo "$OUTFILE successfully created."
+}
+
+ACTION=$1
+shift 1
+
+case "$ACTION" in
+"create")
+   createImageAction "$@"
+   ;;
+"adapter")
+   createVMDKAdapterAction "$@"
+   ;;
+*)
+   usage
+   ;;
+esac
